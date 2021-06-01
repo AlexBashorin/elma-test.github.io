d9a3bdf4b2af7ea51de635672b4173065bf6c303
AlexBashorin <alexander.bashorin@yandex.ru>
2021-06-01 21:53:38 +0400

    start new structure

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const datesContainer = document.querySelector('.dates__container');
 const datesItem = document.querySelector('.dates__item');
+const datesWrapper = document.querySelector('.dates__wrapper');
+
+let startDate = new Date(2021-06-01);
+let endDate = new Date(2021-07-01);
+
+let dateArray = [];
+
+while(startDate < endDate) {
+    startDate.setDate(startDate.getDate() + 1);
+    
+}
 
 
