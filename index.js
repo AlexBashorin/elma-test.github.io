3d3d64ee3dc22d8d9c97dd797fe1c862e4e85f8d
Fabrika Li <alexander.bashorin@yandex.ru>
2021-06-03 18:25:43 +0400

    get request json

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const datesWrapper = document.querySelector('.dates__wrapper');
 let startDate = new Date(2021-05-01);
 let endDate = new Date(2021-07-01);
 
-let dateArray = [];
-let dateItem = document.createElement('DIV');
-datesWrapper.appendChild(dateItem.classList.add('dates__item'));
+// let dateArray = [];
+// let dateItem = document.createElement('DIV');
+// datesWrapper.appendChild(dateItem.classList.add('dates__item'));
 
-while(startDate < endDate) {
-    startDate.setDate(startDate.getDate() + 1);
-}
+// while(startDate < endDate) {
+//     startDate.setDate(startDate.getDate() + 1);
+// }
 
 
 
@@ -79,17 +79,54 @@ cardBacklog.ondragstart = function() {
 
 
 //USERS
-let usersData = JSON.parse(users, function(key, value) {
-    if(key == 'id') return Number;
-    return value;
-});
+let requestUsers = 'https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/users';
+let requestU = new XMLHttpRequest();
+requestU.open('GET', requestUsers);
+requestU.responseType = 'json';
+requestU.send();
+
+requestU.onload = function() {
+    let users = requestU.response;
+    placeUsers(users);
+}
+
+const taskField = document.querySelector('.task-fields');
+let taskItem = document.createElement('DIV');
+taskItem.classList.add('task-fields__item');
+
+function placeUsers(jsonObj) {
+    let taskName = document.createElement('DIV');
+    taskName.classList.add('task-fields__namespace');
+    let paraName = document.createElement('P');
+    paraName.textContent = jsonObj['surname'] + jsonObj['firstName'];
+
+    taskField.appendChild(taskItem);
+    taskItem.appendChild(taskName);
+    taskName.appendChild(paraName);
+}
+
+
+// let usersData = JSON.parse(users, function(key, value) {
+//     if(key == 'id') return Number;
+//     return value;
+// });
 
 //TASKS
-let tasksData = JSON.parse(tasks, function(key, value){
-    if(key == 'creationDate' || key == 'planStartDate' || key == 'planEndDate' || key == 'endDate') return new Date(value);
-    return value;
-});
+let requestTasks = 'https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/tasks';
+let requestT = new XMLHttpRequest();
+requestT.open('GET', requestTasks);
+requestT.responseType = 'json';
+requestT.send();
 
-window.addEventListener("click", function() {
-    alert(tasksData[1].creationDate.getDate());
-})
+requestT.onload = function() {
+    let tasks = requestT.response;
+    placeTasks(tasks);
+}
+
+function placeTasks() {
+    
+}
+// let tasksData = JSON.parse(tasks, function(key, value){
+//     if(key == 'creationDate' || key == 'planStartDate' || key == 'planEndDate' || key == 'endDate') return new Date(value);
+//     return value;
+// });
